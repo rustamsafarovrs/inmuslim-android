4f9be405115d73d0f74985e161af7bea5d8a2e9d
rustamsafarovrs <rustamsafarovme@gmail.com>
2026-05-31 00:35:30 +0500

    feat: disable user register in test builds

--- app/src/main/java/tj/rsdevteam/inmuslim/data/repositories/UserRepository.kt
+++ app/src/main/java/tj/rsdevteam/inmuslim/data/repositories/UserRepository.kt
@@ -4,6 +4,8 @@ import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import tj.rsdevteam.inmuslim.core.Resource
+import tj.rsdevteam.inmuslim.core.config.BuildType
+import tj.rsdevteam.inmuslim.core.config.BuildVars
 import tj.rsdevteam.inmuslim.data.api.Api
 import tj.rsdevteam.inmuslim.data.models.Message
 import tj.rsdevteam.inmuslim.data.models.User
@@ -64,6 +66,9 @@ class UserRepository
     }
 
     fun needRegister(): Boolean {
+        if (BuildVars.BUILD_TYPE == BuildType.TEST) {
+            return false
+        }
         return preferences.getUserId() == -1L
     }
 
